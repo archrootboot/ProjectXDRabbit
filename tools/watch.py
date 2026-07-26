64fe185d9be4122c95fe5fa9a98fae33a5d3156c
archrootboot <archrootboot@gmail.com>
2026-07-27 01:09:39 +0530

    Update watch.py

--- tools/watch.py
+++ tools/watch.py
@@ -227,7 +227,7 @@ def watch_video(driver, udid, stop_event):
                 image_element = wait.until(EC.element_to_be_clickable(
                     (AppiumBy.ID, image_id)
                 ))
-                time.sleep(2)
+                time.sleep(3)
                 image_element.click()
 
                 result = wait_for_video(duration)
