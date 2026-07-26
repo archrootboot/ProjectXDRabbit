15e51f04f560974abb0933e2f74c359ee84e38f0
archrootboot <archrootboot@gmail.com>
2026-07-26 23:06:57 +0530

    Update watch.py

--- tools/watch.py
+++ tools/watch.py
@@ -227,6 +227,7 @@ def watch_video(driver, udid, stop_event):
                 image_element = wait.until(EC.element_to_be_clickable(
                     (AppiumBy.ID, image_id)
                 ))
+                time.sleep(2)
                 image_element.click()
 
                 result = wait_for_video(duration)
@@ -260,7 +261,7 @@ def watch_video(driver, udid, stop_event):
                     (AppiumBy.ID, "com.view.ytrabbit:id/textView_chage")
                 ))
                 skip_button.click()
-                time.sleep(1)
+                time.sleep(2)
 
         except Exception as e:
             consecutive_errors += 1
